0dfdec88861f76cd1e42e6cedf57ec6841059534
Christian Brill <quedarme@gmail.com>
2017-04-06 09:34:31 +0200

    create new route

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,19 @@
 <?php
 
 	$w_routes = array(
+		// nbateams.dev/home/
 		['GET', '/', 'Default#home', 'default_home'],
-
+		// nbateams.dev/contact/
 		['GET', '/contact/', 'Default#contact', 'default_contact'],
-
+		// nbateams.dev/conference/est/
 		['GET', '/conference/est/', 'Conference#est', 'conference_est'],
-
+		// nbateams.dev/conference/ouest/
 		['GET', '/conference/ouest/', 'Conference#ouest', 'conference_ouest'],
-
-		['GET', '/conference/division/[i:id]-[:divName]/', 'Division#division', 'division_division']
+		// nbateams.dev/conference/division/4-sud-est
+		['GET', '/conference/division/[i:id]-[:divName]/', 'Division#division', 'division_division'],
+		//nbateams.dev/signin/
+		['GET', '/signin/', 'User#signin', 'user_signin'],
+		['POST', '/signin/', 'User#signinPost', 'user_signinpost'],
+		//nbateams.dev/signup/
+		//nbateams.dev/forgot_password/
 	);
